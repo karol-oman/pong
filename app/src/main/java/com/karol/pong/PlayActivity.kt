bbcaa1ffac8389a8292a4df76f14ba3650f98fda
h0glund <calle.hoglund@student.kyh.se>
2021-11-29 14:20:01 +0100

    menu and play-activity - done

--- app/src/main/java/com/karol/pong/PlayActivity.kt
+++ app/src/main/java/com/karol/pong/PlayActivity.kt
@@ -3,11 +3,9 @@ package com.karol.pong
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PlayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_play)
     }
 }
