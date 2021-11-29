9dda6ca7f56db4e88ec531bedf283918b9c58d34
karol-oman <karolomanlol@gmail.com>
2021-11-29 11:06:24 +0100

    Test

---
+++ app/src/main/java/com/karol/pong/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.karol.pong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var name: String = "test"
+    }
+}
