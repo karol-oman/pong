ee8012f3db503c54496c9992b36e0a25396c335d
h0glund <calle.hoglund@student.kyh.se>
2021-11-30 12:13:58 +0100

    menu and play-activity - done

--- app/src/main/java/com/karol/pong/MainActivity.kt
+++ app/src/main/java/com/karol/pong/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.karol.pong
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.commit
+import com.karol.pong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.buttonPlay.setOnClickListener(){
+            val intent = Intent(this,PlayActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonSettings.setOnClickListener(){
+            //TODO FRAGMENT HERE
+            supportFragmentManager.commit {
+                add(R.id.frame_layout, SettingsFragment())
+            }
+        }
+
+        binding.buttonScores.setOnClickListener(){
+            //TODO FRAGMENT HERE
+        }
 
     }
 }
--- app/src/main/java/com/karol/pong/PlayActivity.kt
+++ app/src/main/java/com/karol/pong/PlayActivity.kt
@@ -1,11 +1,65 @@
 package com.karol.pong
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.View
+import com.karol.pong.databinding.ActivityMainBinding
+import com.karol.pong.databinding.ActivityPlayBinding
 
-class PlayActivity : AppCompatActivity() {
+class PlayActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTouchListener {
+
+    var circleX : Float = 100f
+    var circleY : Float = 100f
+    val ballPaint: Paint = Paint()
+
+
+    private lateinit var binding: ActivityPlayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play)
+        binding = ActivityPlayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.surfaceView.holder.addCallback(this)
+
+        binding.surfaceView.setOnTouchListener(this)
+
+
+
+    }
+
+    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean{
+        drawBall()
+        return true
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+    }
+
+    private fun drawBall(){
+        val canvas: Canvas? = binding.surfaceView.holder.lockCanvas()
+        val surfaceBackground = Paint()
+        surfaceBackground.color = Color.WHITE
+
+        canvas?.drawRect(0f,0f, binding.surfaceView.width.toFloat(), binding.surfaceView.height.toFloat(), surfaceBackground)
+
+        ballPaint.color = Color.RED
+        canvas?.drawCircle(circleX, circleY, 100f, ballPaint)
+
+        binding.surfaceView.holder.unlockCanvasAndPost(canvas)
+        binding.surfaceView.setZOrderOnTop(true)
     }
 }
