f1353935f805c82c623a0a0e62b4c61986ecf510
h0glund <calle.hoglund@student.kyh.se>
2021-12-03 11:54:48 +0100

    random background fixed

--- app/src/main/java/com/karol/pong/MainActivity.kt
+++ app/src/main/java/com/karol/pong/MainActivity.kt
@@ -1,25 +1,9 @@
 package com.karol.pong
 
-import android.app.Dialog
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View.inflate
-import android.widget.PopupWindow
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.commit
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.navigation.NavigationView
-
 import com.karol.pong.databinding.ActivityMainBinding
-import com.karol.pong.databinding.ActivityMainBinding.inflate
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,45 +14,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonPlay.setOnClickListener(){
-            val intent = Intent(this,PlayActivity::class.java)
+
+        val random = (0..1).random()
+
+        val imgId = arrayOf(R.drawable.backgroundoneblur, R.drawable.treeboardbetter_jpg)
+
+        binding.layoutMain.setBackgroundResource(imgId[random])
+
+
+        binding.buttonPlay.setOnClickListener() {
+            val intent = Intent(this, PlayActivity::class.java)
             startActivity(intent)
         }
 
-        binding.buttonSettings.setOnClickListener(){
+        binding.buttonSettings.setOnClickListener() {
             //TODO INTENT HERE asd
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
 
-        binding.buttonScores.setOnClickListener(){
-
+        binding.buttonScores.setOnClickListener() {
 
 
             val intent = Intent(this, HighScoreActivity::class.java)
             startActivity(intent)
 
-
-
-
-
-
-
-            //window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-
-
-
-
-
-
-
         }
 
     }
-
-
-
 
 
 }
