10d7eda394efa50b2713584e5e7e8ba660db885a
Sarahparah <90765320+Sarahparah@users.noreply.github.com>
2021-12-20 10:43:10 +0100

    highscore

--- app/src/main/java/com/karol/pong/fragments/HighScorePongFragment.kt
+++ app/src/main/java/com/karol/pong/fragments/HighScorePongFragment.kt
@@ -1,19 +1,25 @@
 package com.karol.pong.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.karol.pong.DataController
 import com.karol.pong.R
 
 class HighScorePongFragment: Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         return inflater.inflate(R.layout.layout_high_score_pong, container, false)
+
     }
 
 }
